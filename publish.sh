e1e35b1326a3222f73a3ab1de8b8a11b5d66acb1
Henry Snoek <snoek09@gmail.com>
2015-02-22 15:01:37 +0100

    update script, disable sass compile

--- publish.sh
+++ publish.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
-
-# Replace "sculpin generate" with "php sculpin.phar generate" if sculpin.phar
-# was downloaded and placed in this directory instead of sculpin having been
-# installed globally.
-
-sculpin generate --env=prod
-if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
-
-# Add --delete right before "output_prod" to have rsync remove files that are
-# deleted locally from the destination too. See README.md for an example.
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
-if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
+#!/bin/bash sh
+# make the script fail for any failed command
+set -e
+# make the script display the commands it runs to help debugging failures
+set -x
+# build site
+#sass source/css/wouterj.scss:source/css/wouterj.css --style compressed --no-cache
+./vendor/bin/sculpin generate --env prod
+touch output_prod/.nojekyll
