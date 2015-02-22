219f1e08f8a4eaaf0b46ac897895e3faf7174a62
Henry Snoek <snoek09@gmail.com>
2015-02-22 15:31:10 +0100

    not using sass

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ set -e
 # make the script display the commands it runs to help debugging failures
 set -x
 # build site
-#sass source/css/wouterj.scss:source/css/wouterj.css --style compressed --no-cache
 ./vendor/bin/sculpin generate --env prod
 touch output_prod/.nojekyll
