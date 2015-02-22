d8cac88f72b172a673f41ec15e5f7c7c6ecfa325
Henry Snoek <snoek09@gmail.com>
2015-02-22 15:09:22 +0100

    publish should be build script

--- publish.sh
+++ publish.sh
@@ -1,9 +1,27 @@
-#!/bin/bash sh
+#!/usr/bin/env sh
 # make the script fail for any failed command
 set -e
 # make the script display the commands it runs to help debugging failures
 set -x
-# build site
-#sass source/css/wouterj.scss:source/css/wouterj.css --style compressed --no-cache
-./vendor/bin/sculpin generate --env prod
-touch output_prod/.nojekyll
+
+# Go to the output directory
+cd output_prod
+
+# Remove the existing repo if it exists
+if [ -d ".git" ]; then
+    rm -rf .git
+fi
+
+# Create a repo for the built website for the master branch
+git init
+
+# configure env (locally)
+git config user.email 'snoek09@gmail.com'
+git config user.name 'snoek09 bot'
+
+# commit build
+git add .
+git commit -m "Build website"
+
+# push to GitHub Pages
+git push "https://github.com/snoek09/snoek09.github.com" -f master
