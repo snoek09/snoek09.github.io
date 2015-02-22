c4b6cb71fb0b8d9e2f8d71237ed39eebd3e4b642
Henry Snoek <snoek09@gmail.com>
2015-02-22 21:02:50 +0100

    test without -e option

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ set -x
 # build site
 ./vendor/bin/sculpin generate --env prod
 touch output_prod/.nojekyll
-echo -e "language: php\nphp:\n-hhvm" > output_prod/.travis.yml
+echo "language: php\nphp:\n-hhvm" > output_prod/.travis.yml
