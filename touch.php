89479a9a20303b9ec18b7515f1bad35fbbc44516
Henry Snoek <snoek09@gmail.com>
2016-08-22 20:00:20 +0200

    add helper script to create posts

---
+++ touch.php
@@ -0,0 +1,19 @@
+<?php
+$date = date('Y-m-d');
+if(!empty($argv[2])){
+    $date = $argv[2];
+}
+$file = 'source/_posts/' . $date . '-' . str_replace('_', '-', $argv[1]) . '.md';
+
+$title = ucwords(str_replace('_', ' ', $argv[1]));
+$handle = fopen($file, 'w');
+$data = "---
+title: {$title}
+tags: []
+categories: []
+
+---
+
+";
+fwrite($handle, $data);
+
